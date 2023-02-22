b422ef241bf4148c4c59e37748d451719fb6bc58
Billy <Lrdweaver@gmail.com>
2023-02-21 19:43:11 -0500

    onload function

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-import { getCityWeather, setDate } from './src/functions/dom_manipulation';
+import { onLoad } from './src/functions/dom_manipulation';
 
 import styles from './src/styles.css'
 
 const weatherBtn = document.querySelector('.getWeatherBtn');
 const cityInput = document.querySelector('.city-input')
 
-setDate();
-
 weatherBtn.addEventListener('click', (event) => {
     event.preventDefault();
     getCityWeather(cityInput.value)
 })
 
-getCityWeather('toronto');
+onLoad()
--- src/functions/dom_manipulation.js
+++ src/functions/dom_manipulation.js
@@ -20,7 +20,16 @@ const dayThree = document.querySelector('.day-three')
 const dayFour = document.querySelector('.day-four')
 const dayFive = document.querySelector('.day-five')
 
-let displayWeatherData = (cityWeather) => {
+let onLoad = async () => {
+    let cityCoords = await getCityCoords('santa cruz de tenerife');
+    getCitySun(cityCoords.lat, cityCoords.lon)
+    displayWeatherData('santa cruz de tenerife')
+}
+
+let displayWeatherData = async (cityName) => {
+    let url = fetchCityWeather(cityName)
+    let response = await fetch(url)
+    let cityWeather = await response.json()
     cityTitle.textContent = cityWeather.name;
     currentTemp.textContent = ` ${Math.round(cityWeather.main.temp)} °C`
     weatherDescription.textContent = cityWeather.weather[0].description
@@ -28,51 +37,23 @@ let displayWeatherData = (cityWeather) => {
     dayOneHigh.textContent = Math.round(cityWeather.main.temp_max);
 };
 
-let lon = '-16.2546'
-let lat = '28.4682'
-
 let setDate = () => {
     todaysDate.textContent = formatDate('day')
     todaysTime.textContent = formatDate('time')
-}
+};
 
-let getCityWeather = async (cityName) => {
+let getCityCoords = async (cityName) => {
     let url = fetchCityWeather(cityName);
-    let cityWeather = await fetch(url)
-    cityWeather.json().then((cityWeather) => {
-        displayWeatherData(cityWeather);
-        lon = cityWeather.coord.lon
-        lat = cityWeather.coord.lat
-    })
-    .then (getCitySun());
-}
+    let response = await fetch(url)
+    let cityCoords = await response.json()
+    return cityCoords.coord
+};
 
-/* /let getCityWeather = async () => {
-    console.log(Date)
-    let cityInput = document.querySelector('.cityInput')
-    let cityName = ''
-    if (cityInput.value === '') {
-        cityName = 'Santa Cruz de Tenerife'
-    } else {
-        cityName = cityInput.value;
-    }
-    let cityWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=05f38c988f47ba62b87ed1de9b4136f4&units=metric`)
-    cityWeather.json().then((cityWeather) => {
-        console.log(cityWeather)
-        displayWeatherData(cityWeather);
-        lon = cityWeather.coord.lon
-        lat = cityWeather.coord.lat
-    })
-    await getCitySun()
-    await getFiveDayForecast()
-}; */
-
-let getCitySun = async () => {
+let getCitySun = async (lat, lon) => {
     let citySun = await fetch(`https://api.sunrisesunset.io/json?lat=${lat}&lng=${lon}&timezone=EST&date=today`)
-    citySun.json().then((citySun) => {
-        sunriseTime.textContent = citySun.results.sunrise
-        sunsetTime.textContent = citySun.results.sunset
-    })
-}
+    const sunData = await citySun.json();
+        sunriseTime.textContent = sunData.results.sunrise
+        sunsetTime.textContent = sunData.results.sunset
+};
 
-export { getCityWeather, setDate }
+export { onLoad }
